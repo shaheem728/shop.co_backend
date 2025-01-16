7c5fed9953c4b07bd0b4f1b80dd9d6d97bd3efb5
shaheem mahmood m <140435351+shaheem728@users.noreply.github.com>
2025-01-17 00:06:31 +0530

    Update build_files.sh

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -2,9 +2,13 @@
 echo "BUILD START"
 
 echo "Installing dependencies..."
-python3.12 pip install -r requirements.txt || { echo "Dependency installation failed"; exit 1; }
+pip install -r requirements.txt || { echo "Dependency installation failed"; exit 1; }
+
+echo "Running migrations..."
+python3.12 manage.py makemigrations --noinput || { echo "Migrations failed"; exit 1; }
+python3.12 manage.py migrate --noinput || { echo "Migration application failed"; exit 1; }
 
 echo "Collecting static files..."
-python3.12 manage.py collectstatic --noinput --clear || { echo "Static files collection failed"; exit 1; }
+python3.12 manage.py collectstatic --noinput --clear || { echo "Static file collection failed"; exit 1; }
 
 echo "BUILD END"

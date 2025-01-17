8b076dd016dca1c4dcadb16a19730e6ea7fa93a3
shaheem <shaheemmahmood728@gmail.com>
2025-01-17 12:08:11 +0530

    modified the staticfiles

--- backend/settings.py
+++ backend/settings.py
@@ -180,8 +180,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_DIRS = os.path.join(BASE_DIR,'static'),
-STATIC_ROOT = os.path.join(BASE_DIR / 'staticfiles_build' / 'static')
+STATIC_ROOT = os.path.join(BASE_DIR / 'staticfiles_build')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STRIPE_SECRET_KEY = config('STRIPE_SECRET_KEY')

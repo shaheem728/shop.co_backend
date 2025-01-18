d10e26d30bb63c6056c2596430f20d13ea8529bf
shaheem mahmood m <140435351+shaheem728@users.noreply.github.com>
2025-01-18 20:04:19 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 echo "BUILD START"
+# Install pip if not already installed
 python3 -m ensurepip --upgrade
-pip install --no-cache-dir -r requirements.txt
+python3 -m pip install --upgrade pip
+# Install required dependencies
+python3 -m pip install --no-cache-dir -r requirements.txt
 echo "Collectstatic.."
 python3.9 manage.py collectstatic --noinput --clear
 echo "BUILD END"

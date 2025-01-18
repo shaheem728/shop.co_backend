43aa4ca587016f20d68ebce7e4b728742a12fbca
shaheem mahmood m <140435351+shaheem728@users.noreply.github.com>
2025-01-18 19:05:03 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',cast=bool)
 
-ALLOWED_HOSTS = ['shop-co-backend-rust.vercel.app', '127.0.0.1']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
 
 
 # Application definition

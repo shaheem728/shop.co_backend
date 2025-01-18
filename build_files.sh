aa21650c9cc229d2bd96dda2429ddafeb41b486e
shaheem mahmood m <140435351+shaheem728@users.noreply.github.com>
2025-01-18 18:06:24 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 echo "BUILD START"
-python3.9 pip install -r requirements.txt
-echo "make migrations"
-python3.9 manage.py makemikrations
-echo "migrate"
-python3.9 manage.py migrate
-echo "collectstatic"
+python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput --clear
 echo "BUILD END"

62c2cf558ba677df0a8a39c5738167823ba3cc8e
shaheem mahmood m <140435351+shaheem728@users.noreply.github.com>
2025-01-18 19:47:42 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "BUILD START"
-pip install -r requirements.txt
+python3.9 pip install -r requirements.txt
 echo "Collectstatic.."
 python3.9 manage.py collectstatic --noinput --clear
 echo "BUILD END"

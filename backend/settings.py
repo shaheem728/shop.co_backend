aa9e8a80932e0b9dc0e8d205ba68bb2f98b4eb97
shaheem mahmood m <140435351+shaheem728@users.noreply.github.com>
2025-01-29 23:09:30 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -182,7 +182,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/staticfiles_build/'
 STATIC_ROOT = os.path.join(BASE_DIR / 'staticfiles_build')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')

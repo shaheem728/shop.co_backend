45e99f5dac7429486c4a00f96c4998b3a8c0d847
shaheem mahmood m <140435351+shaheem728@users.noreply.github.com>
2025-02-09 20:47:29 +0530

    Update urls.py

--- product/urls.py
+++ product/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns=[
-    path('products/',views.ProductListviews.as_view()),
+    path('products/',views.ProductListview.as_view()),
     path('review/<str:uuid>',views.ReviewListView.as_view()),
     path('createreview/',views.ReviewCreateView.as_view())
 ]
